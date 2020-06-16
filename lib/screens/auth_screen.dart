2fb67d472f90b73f7c33ca7b399e5c88b754261b
Fabian <fabian.rodriguez@cl1cky.com>
2020-06-16 16:23:37 -0500

    third commit

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../widgets/auth_form.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
+
+  void _submitForm(
+      String email, String password, String username, String date, String city, String phone, bool isLogin, ctx) async {
+    AuthResult authResult;
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData({
+          'name': username,
+          'email': email,
+          'city': city,
+          'phone': phone,
+          'birthday': date,
+        });
+      }
+    } on PlatformException catch (err) {
+      var message = 'An error ocurred';
+      if (err.message != null) {
+        message = err.message;
+      }
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (err) {
+      print(err);
+
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(_isLoading, _submitForm),
+    );
+  }
+}
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,212 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/profile_image.dart';
+
+class ProfileScreen extends StatelessWidget {
+  void _signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+        elevation: 0,
+      ),
+      body: FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (ct, userSnap) {
+          if (userSnap.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return StreamBuilder(
+            stream: Firestore.instance
+                .collection('users')
+                .document(userSnap.data.uid)
+                .snapshots(),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              final document = snapshot.data;
+              return SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      width: double.infinity,
+                      color: Color(0xFF191919),
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 22,
+                          vertical: 16,
+                        ),
+                        child: Stack(
+                          children: <Widget>[
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                ProfileImage(
+                                    document['image'],
+                                    userSnap.data.uid,
+                                    snapshot.data['comments']),
+                                const SizedBox(width: 16),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    const SizedBox(height: 22),
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          document['name'],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 22,
+                                          ),
+                                        ),
+                                        SizedBox(width: 5),
+                                        Icon(
+                                          Icons.check_circle,
+                                          size: 22,
+                                          color: Colors.white,
+                                        )
+                                      ],
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      document['email'],
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    'Escritor',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Spacer(flex: 1),
+                                  Image.asset(
+                                    'assets/fb.png',
+                                    width: 32,
+                                  ),
+                                  SizedBox(width: 16),
+                                  Image.asset(
+                                    'assets/twitter_icon.png',
+                                    width: 32,
+                                  ),
+                                  SizedBox(width: 16),
+                                  Image.asset(
+                                    'assets/ig.png',
+                                    width: 32,
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.person,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Seguidores',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Text('210'),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.person,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Votantes',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Text('870'),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.insert_chart,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Encuestas creadas',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Text('52'),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.poll,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Encuestas contestadas',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Text(document['votes'] == null
+                          ? '0'
+                          : '${document['votes'].length}'),
+                    ),
+                    Divider(),
+                    ListTile(
+                      onTap: _signOut,
+                      title: Text(
+                        'Cerrar sesión',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
